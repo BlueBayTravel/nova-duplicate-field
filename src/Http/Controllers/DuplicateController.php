a4c0392abd2f0b3b306b62aee790838eecfb95b0
Craig Harley <craigharley2@googlemail.com>
2019-08-28 13:31:35 +0100

    progress, but it's not working. Having a look at another issue

--- src/Http/Controllers/DuplicateController.php
+++ src/Http/Controllers/DuplicateController.php
@@ -9,22 +9,30 @@ class DuplicateController extends Controller
 {
     /**
      * Duplicate a nova field and all of the relations defined.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return array
      */
     public function duplicate(Request $request)
     {
+        $basePath = config('nova.path') === '/' ?
+            config('app.url') :
+            config('nova.path');
+
         // Replicate the model
         $model = $request->model::where('id', $request->id)->first();
 
         if (!$model) {
             return [
-                'status' => 404,
-                'message' => 'No model found.',
-                'destination' => config('nova.url') . config('nova.path') . '/resources/' . $request->resource . '/'
+                'status'      => 404,
+                'message'     => 'No model found.',
+                'destination' => $basePath.'/resources/'.$request->resource.'/'
             ];
         }
 
+        /** @var \Illuminate\Database\Eloquent\Model $model */
         $newModel = $model->replicate();
-        $newModel->push();
 
         if (isset($request->relations) && !empty($request->relations)) {
             // load the relations
@@ -35,23 +43,24 @@ class DuplicateController extends Controller
                 foreach ($items as $item) {
                     // clean up our models, remove the id and remove the appends
                     unset($item->id);
+                    /** @var \Illuminate\Database\Eloquent\Model $item */
                     $item->setAppends([]);
 
                     // create a relation on the new model with the data.
+                    dd($item->toArray());
+
                     $newModel->{$relation}()->create($item->toArray());
                 }
             }
         }
 
-        $basePath = config('nova.path') === '/' ?
-            config('app.url') :
-            config('nova.path');
+        $newModel->push();
 
         // return response and redirect.
         return [
-            'status' => 200,
-            'message' => 'Done',
-            'destination' => url($basePath . '/resources/' . $request->resource . '/' . $newModel->id)
+            'status'      => 200,
+            'message'     => 'Done',
+            'destination' => url($basePath.'/resources/'.$request->resource.'/'.$newModel->id)
         ];
     }
 }
